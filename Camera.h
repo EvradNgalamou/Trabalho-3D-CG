1677212043754d1fa0e490d0b7c67175312b86c5
gibbs2b <guilhermegoldnerdasilva@gmail.com>
2023-12-08 18:42:14 -0300

    Fix angulo inicial da camera

--- Camera.h
+++ Camera.h
@@ -12,7 +12,7 @@ class Camera {
     GLfloat camDist = 15;
 
     // Angulos de rotação da camera
-    GLfloat horizontalAngle = 90; // horizontal
+    GLfloat horizontalAngle = 1.57; // horizontal
     GLfloat verticalAngle = 0; // vertical
 
     int toggleCam = 0;
