d669226a8d61a74abe126d856d7524d037cc4430
gibbs2b <guilhermegoldnerdasilva@gmail.com>
2023-12-08 14:23:36 -0300

    Mover arma para frente

--- volumes.cpp
+++ volumes.cpp
@@ -190,7 +190,7 @@ void DesenhaJogadorDeTeste()
 
     // Desenho Arma
     glPushMatrix();
-    glTranslatef(2, 0, 6);
+    glTranslatef(2, 1, 6);
     glScalef(0.5, 4, 0.5);
     glutSolidCube(1);
     glPopMatrix();
