cc8d2cc26053ef430e531c8cd53b8a125f23ce56
gibbs2b <guilhermegoldnerdasilva@gmail.com>
2023-12-08 14:03:47 -0300

    Mudar camera para usar botão do meio para mexer a câmera

--- Camera.cpp
+++ Camera.cpp
@@ -8,12 +8,12 @@
 #include <GL/glut.h>
 
 void Camera::onMouseKey(int button, int state, int x, int y) {
-    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+    if (button == GLUT_MIDDLE_BUTTON && state == GLUT_DOWN) {
         lastX = x;
         lastY = y;
         buttonDown = 1;
     }
-    if (button == GLUT_LEFT_BUTTON && state == GLUT_UP) {
+    if (button == GLUT_MIDDLE_BUTTON && state == GLUT_UP) {
         buttonDown = 0;
     }
 }
