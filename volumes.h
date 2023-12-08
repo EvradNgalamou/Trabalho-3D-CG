22726592c55b3a17efc72a9969dd0b0a05e9a3a6
Alan do Espirito Santo Vitorino <alanesvitorino@hotmail.com>
2023-12-08 02:27:43 -0300

    Inclusão LoadTextureRAW

--- volumes.h
+++ volumes.h
@@ -5,10 +5,15 @@
 #include <GL/glu.h>
 #include <GL/glut.h>
 
+# include "imageloader.h"
+
 void DrawAxes(double size);// FROM opengl3D.cpp laboratorio 4
 
 void DrawSolideCube (double size);
 
 void DrawSolideSphere (double radius , GLint slices , GLint stacks);
 
+void DisplayPlane (GLuint texture, GLfloat Dx, GLfloat Dy, GLfloat Dz);
+
+GLuint LoadTextureRAW( const char * filename );
 #endif
